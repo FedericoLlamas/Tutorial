ef086e4248af6848ee62d693acddc33ef2cc27f5
Federico Llamas <fede.llamas@gmail.com>
2016-11-19 13:09:23 -0300

    init step 8

---
+++ app/src/main/java/ar/edu/unc/famaf/redditreader/ui/NewsDetailActivity.java
@@ -0,0 +1,13 @@
+package ar.edu.unc.famaf.redditreader.ui;
+
+/**
+ * Created by federico on 19/11/16.
+ */
+
+public class NewsDetailActivity {
+
+    public NewsDetailActivity() {
+        super();
+    }
+
+}
---
+++ app/src/main/java/ar/edu/unc/famaf/redditreader/ui/NewsDetailActivityFragment.java
@@ -0,0 +1,18 @@
+package ar.edu.unc.famaf.redditreader.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by federico on 19/11/16.
+ */
+
+public class NewsDetailActivityFragment extends Fragment {
+
+    public NewsDetailActivityFragment() {
+    }
+
+}
